a0e43311d6799d1246ae192f474a287abe08466b
Gianfranco Mariotti <gianfranco.mariotti94@gmail.com>
2019-10-19 18:45:43 +0200

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -15,13 +15,12 @@ BigFloat factorial(const BigFloat& n)
 int main()
 {	
 	//200! takes 5 sec
-	//300! takes 20 sec,	615 cifre								--> compiled with -O6 1 sec
-	//500! takes 115 sec, 	1135 cifre,	memoria fisica 23% --> 29%	--> compiled with -O6 4 sec
-	//1000! takes 1200 sec,	2568 cifre,	memoria fisica 23% --> 70%	--> compiled with -O6 47 sec
+	//300! takes 20 sec,	615 cifre	--> compiled with -O6: 1 sec
+	//500! takes 115 sec, 	1135 cifre,	--> compiled with -O6: 4 sec
+	//1000! takes 1200 sec,	2568 cifre,	--> compiled with -O6: 47 sec
 	BigFloat fact,num(1000);
 	fact = factorial(num);
 	std::cout<<fact<<std::endl;
 	std::cout<<fact.Ints()<<std::endl;
-	system("PAUSE");
 	return 0;
 }
